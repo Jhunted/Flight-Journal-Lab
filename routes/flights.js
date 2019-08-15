e57e1969bfa5a4e4fde18f7f1e7fd89ae2ec1b85
myDeveloperJourney <daniel@mydeveloperjourney.com>
2019-08-15 09:58:23 -0500

    adds boilerplate for routes

---
+++ routes/flights.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const flightCtlr = require('../controllers/flights');
+
+// GET /flights/ => "See all the flights"
+// Routers map to a controller action
+
+router.get('/', flightCtlr.index); 
+
+module.exports = router;
