8491c850e002f57fd457eb424dba8f1c184d12e8
myDeveloperJourney <daniel@mydeveloperjourney.com>
2019-08-16 15:54:49 -0500

    adds route to map to new ctlr action

--- routes/flights.js
+++ routes/flights.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const flightCtlr = require('../controllers/flights');
 
+// => /flights
+
 // GET /flights/ => "See all the flights"
 // Routers map to a controller action
 
 router.get('/', flightCtlr.index); 
+
+// GET /flights/new => renders the flight form to the client so a flight can then be created
+
+router.get('/new', flightCtlr.new);
 
 module.exports = router;
