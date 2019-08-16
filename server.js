3376b80c5ab903ffb6720e46d22a6828f94542a6
myDeveloperJourney <daniel@mydeveloperjourney.com>
2019-08-16 16:07:06 -0500

    mounts body parsing middleware

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ require('./config/mongoose');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// make sure you add your body parsing middleware above your router middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 app.use('/flights', flightsRouter);
